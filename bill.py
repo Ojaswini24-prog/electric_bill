13ebe8b8b527a1cc8803e69dc2b4c9f0632c834f
Ojaswini Khapre <ojaswinikhapre24@gmail.com>
2025-10-14 21:14:36 +0530

    Updated files

--- bill.py
+++ bill.py
@@ -3,12 +3,12 @@ def calculate_bill(units):
         return 0
     elif units <= 200:
         return (units - 100) * 5
-    elif units <= 300:                                                
+    elif units <= 300:
         return (100 * 5) + (units - 200) * 10
     else:
         return (100 * 5) + (100 * 10) + (units - 300) * 20
-    
+
 for i in range(1, 99): 
-    units = int(input(f"\nEnter consumed units {i}:"))
-    
-print("Total Bill: ₹", calculate_bill(units))    
+    units = int(input(f"\nEnter consumed units for bill {i}: "))
+
+    print("Total Bill: ₹", calculate_bill(units))
